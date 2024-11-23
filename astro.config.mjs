6bafb9803f369ede2600bbfdb62db329dd865bb3
mchoquette1984 <mchoquette@catapultecommunication.com>
2024-11-23 09:57:41 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
 
-console.log('PUBLIC_EMAILJS_KEY:', process.env.PUBLIC_EMAILJS_KEY);
-console.log('PUBLIC_EMAILJS_SERVICE_ID:', process.env.PUBLIC_EMAILJS_SERVICE_ID);
-console.log('PUBLIC_EMAILJS_TEMPLATE_ID:', process.env.PUBLIC_EMAILJS_TEMPLATE_ID);
-console.log('PUBLIC_TURNSTILE_KEY:', process.env.PUBLIC_TURNSTILE_KEY);
-
 export default defineConfig({
   site: 'https://martincscott.com',
   base: '/',
