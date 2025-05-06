b6124a5c8133c6e023fcde8de7eee53c9ae06858
mchoquette1984 <mchoquette@catapultecommunication.com>
2025-05-06 12:22:17 -0400

    colros

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,14 +5,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        heading: '#1a202c', // Light theme heading color
-        paragraph: '#353d4a', // Light theme paragraph color
+        'heading': '#1a202c', // Light theme heading color
+        'paragraph': '#353d4a', // Light theme paragraph color
         'heading-dark': '#e2e8f0', // Dark theme heading color
         'paragraph-dark': '#dce2e8', // Dark theme paragraph color
         'deep-purple': '#0C0512',
         "semi-deep-purple": '#211B26',
         'rose-flash': '#D45CFE',
-        primary: '#3b82f6', // blue-500
+        'custom-orange': '#ff7d54',
+        'custom-cyan': '#16a4c8',
+        'custom-pink': '#eb75d8',
+        'custom-purple': '#b845ed',
+
+        'primary': '#3b82f6', // blue-500
       },
     },
   },
